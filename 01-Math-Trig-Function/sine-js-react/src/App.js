388e5c85acf02346f52cc08f3582707ba4f00c88
Ubuntu <contra@contra-on-azure.e55tgeglle0ebeelgndnrwwnuc.hx.internal.cloudapp.net>
2019-07-06 03:31:34 +0000

    add Math trig sine function using react hooks and svg

---
+++ 01-Math-Trig-Function/sine-js-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Sine from './Sine';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Sine/>
+    </div>
+  );
+}
+
+export default App;
