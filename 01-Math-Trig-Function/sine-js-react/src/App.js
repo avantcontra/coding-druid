e538b7c3b7dc259cb549a8c10bbf8ec8e1296937
Ubuntu <contra@contra-on-azure.e55tgeglle0ebeelgndnrwwnuc.hx.internal.cloudapp.net>
2019-07-06 04:55:58 +0000

    update infomation and style

--- 01-Math-Trig-Function/sine-js-react/src/App.js
+++ 01-Math-Trig-Function/sine-js-react/src/App.js
@@ -7,8 +7,12 @@ function App() {
   return (
     <div className="App">
       <div className="App-info">
-        <p>Forked from <a href="https://codepen.io/HunorMarton/pen/ggQGqQ" target="_blank">codepen.io/HunorMarton/pen/ggQGqQ</a></p>
-        <p>Replaced class components using function and Hooks</p>
+        <p>Sine visualization using svg and React Hooks</p>
+        <p>
+          Forked from <a href="https://codepen.io/HunorMarton/pen/ggQGqQ" target="_blank">codepen.io/HunorMarton/pen/ggQGqQ</a>
+          <br/>
+          Replaced class components using function and Hooks
+        </p>
         <p>by Contra <a href="https://www.floatbug.com" target="_blank">floatbug.com</a></p>
       </div>
       <Sine />
