aed24ecd0432291ec8808a73834c935ab92fb486
avantcontra <dadadaluo@gmail.com>
2019-08-30 21:34:58 +0800

    change file name

--- 02-Math-Fourier-Series/fourier-series-js-react/src/App.js
+++ 02-Math-Fourier-Series/fourier-series-js-react/src/App.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import './App.css';
-import Sine from './Sine';
+import Sine from './Fourier';
 
 
 function App() {
   return (
     <div className="App">
       <div className="App-info">
-        <p>Sine visualization using svg and React Hooks</p>
-        <p>
-          Forked from <a href="https://codepen.io/HunorMarton/pen/ggQGqQ" target="_blank">codepen.io/HunorMarton/pen/ggQGqQ</a>
-          <br/>
-          Replaced class components using function and Hooks
-        </p>
+        <p>Fourier series visualization using svg and React Hooks</p>
+        
         <p>by Contra <a href="https://www.floatbug.com" target="_blank">floatbug.com</a></p>
       </div>
       <Sine />
